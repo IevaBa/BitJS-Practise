acb067a756fbdfd3021f63d8845e933be9103174
Ieva Baltriukaite <ieva.baltriukaite@gmail.com>
2022-04-05 23:05:36 +0300

    commit

---
+++ scripts/conditionals.js
@@ -0,0 +1,100 @@
+/* console.log("==== Task 1 ====");
+let n = 20;
+if (n >= 0) {
+  console.log("Geras");
+} else {
+  console.log("Blogas");
+} */
+
+/* console.log("==== Task 2 ====");
+let n = 2;
+if (n === 1) {
+  console.log("Eikite");
+} else if (n === 2) {
+  console.log("Palaukite");
+} else {
+  console.log("Eiti draudžiama");
+} */
+
+/* console.log("==== Task 3 ====");
+let n = 5;
+let k = 8;
+let d = 3;
+if (n >= k / d) {
+  console.log("Knygos telpa į dėžes");
+} else {
+  console.log("Knygos netelpa į dėžes");
+} */
+
+/* console.log("==== Task 4 ====");
+let a = 8;
+let b = 4;
+if (a > b) {
+  console.log(a - 1);
+  console.log(b + 1);
+} else {
+  console.log(a + 1);
+  console.log(b - 1);
+} */
+
+/* console.log("==== Task 5 ====");
+let c = 85;
+let p = 20;
+if ((k = c / p)) {
+  console.log(Math.floor(k));
+  console.log(c % p);
+} */
+
+/* console.log("==== Task 6 ====");
+let n = 4;
+if (Number.isInteger(n / 3)) {
+  console.log("Trikampį sudėlioti galima");
+} else {
+  console.log("Trikampį sudėlioti negalima");
+} */
+
+/* console.log("==== Task 7 ====");
+let n = 12;
+if (
+  n === 1 ||
+  n === 3 ||
+  n === 5 ||
+  n === 7 ||
+  n === 8 ||
+  n === 10 ||
+  n === 12
+) {
+  console.log(31);
+} else if (n === 2) {
+  console.log(28);
+} else if (n === 4 || n === 6 || n === 9 || n === 11) console.log(30);
+else {
+  console.log("Toks mėnesis neegzistuoja");
+} */
+
+/* console.log("==== Task 8 ====");
+let k = 1;
+if (k === 1 || k === 3 || k === 5) {
+  console.log("Kambarį tvarkys jaunėlis");
+} else if (k === 2 || k === 4 || k === 6) {
+  console.log("Kambarį tvarkys vyresnėlis");
+} else {
+  console.log("Skaičius turi būti nuo 1 iki 6");
+} */
+
+//console.log("==== Task 9 ====");
+/*let m = 1904;
+for (m = 1896; m < 2022; m = m + 4) {
+  console.log(m);
+}
+{
+  console.log("Metai neolimpiniai");
+}
+//} */
+
+console.log("==== Task 10 ====");
+let v1 = 8;
+let m1 = 29;
+let m2 = 43;
+let v = 9;
+let m = 5;
---
+++ scripts/loops.js
@@ -0,0 +1,35 @@
+console.log("==== Task 1 ====");
+for (let i = 0; i < 5; i++) {
+  console.log("Labas");
+}
+
+console.log("==== Task 2 ====");
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+console.log("==== Task 3 ====");
+for (let i = 0; i <= 40; i = i + 10) {
+  console.log(i);
+}
+console.log("==== Task 4 ====");
+for (let i = 49; i <= 53; i++) {
+  console.log(i);
+}
+console.log("==== Task 5 ===="); //????
+for (let i = 0; i < 5; i++) {
+  console.log((i = Math.floor(Math.random(i) * 10)));
+}
+console.log("==== Task 6 ===="); // bandžiau su slice išimti, bet nepavyko
+for (let i = 1; i <= 20; i++) {
+  if (Number.isInteger(i / 3)) {
+    console.log("-");
+  }
+  console.log(i);
+}
+console.log("==== Task 7 ====");
+let n = 10;
+for (let i = 1; i <= 10; i++) {
+  d = i * n;
+  console.log(d);
+}
+console.log("==== Task 8 ===="); //to be continued
