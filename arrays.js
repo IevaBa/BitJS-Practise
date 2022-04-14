ec3befebc6456655d92c097305ce26d58c1a22d9
Ieva Baltriukaite <ieva.baltriukaite@gmail.com>
2022-04-14 14:21:03 +0300

    Finished

--- arrays.js
+++ arrays.js
@@ -14,8 +14,9 @@ console.log(arr); */
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 let arr = [];
-for (let i = 0; i < rand(40, 60); i++) {
-  arr.push(rand(1,arr.length));
+let arrLength = Math.floor(Math.random() * (60 - 40 + 1)) + 40;
+for (let i = 0; i < arrLength; i++) {
+  arr.push(rand(1, arrLength));
 }
 console.log(arr); */
 
@@ -52,59 +53,63 @@ console.log(arr) */
 // Sukurti masyvą, kurio ilgis 100, o reikšmės - atsitiktinės raidės M,N,O,P. Suskaičiuoti, kurios raidės buvo daugiausia.
 
 /* function rand(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);}
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-let raides = ["M","N","O","P"];
+let raides = ["M", "N", "O", "P"];
 let arr = [];
-for (i=0; i <100; i++){
-  arr.push(raides[rand(0,3)]);
+for (i = 0; i < 100; i++) {
+  arr.push(raides[rand(0, 3)]);
 }
 console.log(arr);
 
-let countM=0;
-let countN=0;
-let countO=0;
-let countP=0;
+let countM = 0;
+let countN = 0;
+let countO = 0;
+let countP = 0;
 
- for (i=0; i<arr.length; i++)
-{
+for (i = 0; i < arr.length; i++) {
   if (arr[i] === "M") {
     countM++;
-  } if (arr[i] === "N") {
+  }
+  if (arr[i] === "N") {
     countN++;
-  } if (arr[i] === "O") {
+  }
+  if (arr[i] === "O") {
     countO++;
-  } if (arr[i] === "P") {
+  }
+  if (arr[i] === "P") {
     countP++;
   }
 }
- console.log(countM)
- console.log(countN)
- console.log(countO)
- console.log(countP)
- let max = [countM, countN, countO, countP]
+console.log(countM);
+console.log(countN);
+console.log(countO);
+console.log(countP);
+let max = [countM, countN, countO, countP];
 
- if (Math.max(...max) === countM) {
-   console.log ("Daugiausia yra raidžių M: " + countM)
- } else if (Math.max(...max) === countN) {
-   console.log ("Daugiausia yra raidžių N: " + countN)
- } else if (Math.max(...max) === countO) {
-   console.log ("Daugiausia yra raidžių O: " + countO)
- } else if (Math.max(...max) === countP) {
-   console.log ("Daugiausia yra raidžių P: " + countP)
- } else {
-   console.log ("Kelių raidžių vienodos skaičius")
- } //kai vienodas skaičius neveikia */
+if (Math.max(...max) === countM) {
+  console.log("Daugiausia yra raidžių M: " + countM);
+}
+if (Math.max(...max) === countN) {
+  console.log("Daugiausia yra raidžių N: " + countN);
+}
+if (Math.max(...max) === countO) {
+  console.log("Daugiausia yra raidžių O: " + countO);
+}
+if (Math.max(...max) === countP) {
+  console.log("Daugiausia yra raidžių P: " + countP);
+} */
 
 //console.log("==== Task 6 ====");
 // Sugeneruoti masyvą, kurio ilgis 20, o reikšmės masyve turi būti nuo 10 iki 50. Reikšmės masyve turi būti unikalios, tai yra – nesikartoti.
- 
-/* let n =20;
-let arr= [];
+
+/* let n = 20;
+let arr = [];
 do {
-  let randomNumber = Math.floor(Math.random()* 40)+10;
+  let randomNumber = Math.floor(Math.random() * 41) + 10;
   if (!arr.includes(randomNumber)) {
-  arr.push(randomNumber);
-  }}
-while (arr.length < n);
-console.log(arr) */
+    arr.push(randomNumber);
+  }
+} while (arr.length < n);
+console.log(arr); */
